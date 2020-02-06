1d86da308038ed396c98d82e1d5870e439832dc4
kswanson25 <katieswanson259@gmail.com>
2020-02-05 23:34:21 -0800

    Run Server, Clone Four Services, Port 4000

--- server/index.js
+++ server/index.js
@@ -4,7 +4,17 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 4000;
 const photoGallery = express();
 
+photoGallery.use(express.static('public'));
+
+photoGallery.use(bodyParser.json());
+photoGallery.engine('html', ejs.renderFile);
+
+photoGallery.get('/:id', (req, res) => {
+  res.render('../public/index.html');
+});
+photoGallery.set('view engine', 'html');
+
 
 photoGallery.listen(port, () => {
-  console.log(`Proxy Server Listening on port ${port} `)
+  console.log(`Proxy Server Listening on Port ${port} `)
 })
